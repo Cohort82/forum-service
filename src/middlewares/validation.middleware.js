076328c12a33d8514e993b8c3b238c5cf478ff62
edward <edwardlfish@gmail.com>
2026-06-22 15:29:48 +0300

    Add validation middleware with schema for `createPost`

---
+++ src/middlewares/validation.middleware.js
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+const schema = {
+    createPost: Joi.object({
+        title: Joi.string().required(),
+        content: Joi.string().required(),
+        tags: Joi.array().items(Joi.string())
+    }),
+    // TODO: Add validation for other routes
+}
