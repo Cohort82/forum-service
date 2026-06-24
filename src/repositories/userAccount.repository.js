389351444cd29dd8b26b2158d9cd188c82c1ea9a
edward <edwardlfish@gmail.com>
2026-06-24 10:47:51 +0300

    Implement `changeRoles` service method and add `addRole`/`removeRole` repository methods

--- src/repositories/userAccount.repository.js
+++ src/repositories/userAccount.repository.js
@@ -5,3 +5,7 @@ export const addUser = async (user) => UserAccount.create(user);
 export const removeUser = async (login) => UserAccount.findByIdAndDelete(login, {returnDocument: 'after'}).exec();
 
 export const updateUser = async (login, updateData) => UserAccount.findByIdAndUpdate(login, updateData, {returnDocument: 'after'}).exec();
+
+export const addRole = async (login, role) => UserAccount.findByIdAndUpdate(login,{$addToSet: {roles: role}}, {returnDocument: 'after'}).exec();
+
+export const removeRole = async (login, role) => UserAccount.findByIdAndUpdate(login,{$pull: {roles: role}}, {returnDocument: 'after'}).exec();
